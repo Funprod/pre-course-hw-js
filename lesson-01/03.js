c0b748247c3d0be4c6c5bfc208f1603ea279157a
Funprod <131615685+Funprod@users.noreply.github.com>
2024-05-31 12:12:14 +0300

    Update 03.js

--- lesson-01/03.js
+++ lesson-01/03.js
@@ -1,10 +1,10 @@
-// code
-// let number1 = Math.floor(Math.random() * 100);
-// console.log(
-//   number1 % 2 === 1
-//     ? number1 + ": Это нечетное число"
-//     : number1 + ": Это четное число"
-// );
+code;
+let number1 = Math.floor(Math.random() * 100);
+console.log(
+  number1 % 2 === 1
+    ? number1 + ": Это нечетное число"
+    : number1 + ": Это четное число"
+);
 
 let number = 1;
 
